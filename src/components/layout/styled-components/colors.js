5daaeb530e7edf91798d3dd5a9609dae43026486
Vinicius Arruda <viniarruda.souza@gmail.com>
2019-03-21 17:40:26 +0000

    New colors

--- src/components/layout/styled-components/colors.js
+++ src/components/layout/styled-components/colors.js
@@ -9,7 +9,10 @@ const colors = {
     primary:    '#559FBF',
     secondary:  '#3682A3',
     lighteen:   '#B0DBEE',
-    dark:       '#1D6F93'
+    dark:       '#1D6F93',
+    bg:         '#1e1e1e',
+    header:     '#333333',
+    gradient:   'background: rgb(187,107,217);background: linear-gradient(0deg, rgba(187,107,217,1) 0%, rgba(142,126,251,1) 100%);'
 }
 
 export default colors
--- src/components/views/checkout/index.js
+++ src/components/views/checkout/index.js
@@ -6,13 +6,13 @@ const Checkout = (props) => {
 	return (
 		<div>
 			<h1>Checkout</h1>
-			<h2>Bem vindo {user.name}</h2>
+			<h2>Welcome {user.name}</h2>
 			<div>
 				<h3>Items in cart:</h3>
 				{
-					items.map((i, key) => 
+					items ? items.map((i, key) => 
 						<div key={key}>{i.item}</div>
-					)
+					) : <span>Empty Cart</span>
 				}
 			</div>
 		</div>
