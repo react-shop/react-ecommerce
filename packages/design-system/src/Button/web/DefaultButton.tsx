b1b9eaecbb58517c950a0e0c09dc7b1e3eb2651c
Vinicius <viniarruda.souza@gmail.com>
2019-12-29 13:06:03 -0300

    feat: add default button configs

---
+++ packages/design-system/src/Button/web/DefaultButton.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import theme from '../../theme';
+
+export interface IButtonProps {
+  full?: boolean;
+  secondary?: boolean;
+}
+
+const DefaultButton = styled.div<IButtonProps>`
+  padding: 15px 55px;
+  border-radius: 6px;
+  width: ${(props) => (props.full ? '100%' : 'auto')};
+  text-align: center;
+  font-family: proxima-nova, sans-serif;
+  font-size: ${theme.fonts.sizes.body};
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: ${(props) => (props.secondary ? 'none' : '0px 11px 16px rgba(40, 245, 190, 0.2)')};
+    opacity: ${(props) => (props.secondary ? 0.8 : 1)}
+  }
+`;
+
+export default DefaultButton;
--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -1,1 +1,2 @@
 export { Button } from './Button';
+export { Container } from './Grid';
