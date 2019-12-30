c317a861021b841c3596bedeeefa90fa38def483
Vinicius <viniarruda.souza@gmail.com>
2019-12-30 12:00:20 -0300

    feat: add a11y addon to storybook

--- packages/design-system/.storybook/config.js
+++ packages/design-system/.storybook/config.js
@@ -1,7 +1,10 @@
-import { configure, addParameters } from '@storybook/react';
+import { configure, addParameters, addDecorator } from '@storybook/react';
 import { themes } from '@storybook/theming';
+import { withA11y } from '@storybook/addon-a11y';
 
 const req = require.context('../src', true, /\.story\.(ts|tsx)$/);
+
+addDecorator(withA11y);
 
 addParameters({
   options: {
