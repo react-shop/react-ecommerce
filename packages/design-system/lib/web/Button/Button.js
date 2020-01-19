4ba15b86cb3c29f5f116e55c25397d5cf62fc4f1
MateusAndrade <mateus.andrade47@outlook.com>
2020-01-19 12:55:54 -0300

    build: adding new build for Button

--- packages/design-system/lib/web/Button/Button.js
+++ packages/design-system/lib/web/Button/Button.js
@@ -7,6 +7,6 @@ var react_1 = __importDefault(require("react"));
 var StyledButton_1 = __importDefault(require("./StyledButton"));
 var OutlinedButton_1 = __importDefault(require("./OutlinedButton"));
 exports.Button = function (_a) {
-    var children = _a.children, outline = _a.outline, full = _a.full, secondary = _a.secondary, onClick = _a.onClick;
+    var children = _a.children, _b = _a.outline, outline = _b === void 0 ? false : _b, _c = _a.full, full = _c === void 0 ? false : _c, _d = _a.secondary, secondary = _d === void 0 ? false : _d, onClick = _a.onClick;
     return (react_1["default"].createElement(react_1["default"].Fragment, null, outline ? (react_1["default"].createElement(OutlinedButton_1["default"], { full: full, onClick: onClick, secondary: secondary }, children)) : (react_1["default"].createElement(StyledButton_1["default"], { full: full, onClick: onClick, secondary: secondary }, children))));
 };
