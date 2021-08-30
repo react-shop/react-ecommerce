bc0088e28ac9566a17559079f5f2dba79408b6b7
MateusAndrade <mateus.andrade47@outlook.com>
2021-08-30 15:01:55 -0300

    feat: adding docker-compose + pgadmin

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -23,7 +23,8 @@ import { AppController } from './app.controller';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_HOST,
-      port: (process.env.DATABASE_PORT as unknown) as number,
+      // eslint-disable-next-line prettier/prettier
+      port: process.env.DATABASE_PORT as unknown as number,
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
